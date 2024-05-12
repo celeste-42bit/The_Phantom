ba27111d46c6a0c18e807795e7fed8631cccbc24
caelestia-42bit <studi.jasha.gruener@gmail.com>
2024-05-13 00:06:01 +0200

    Added logging

--- app.py
+++ app.py
@@ -16,15 +16,40 @@ activity = Activity(name="over this server", type=ActivityType.watching)
 
 client = Client(intents = intents)
 
-if(config['app']['discord_api']['logging']):
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-else:
-    handler = None
+# Following lines set up the logger
+activate_logging = config['app']['logging']['activate']
+logging_level = str(config['app']['logging']['level'])
 
+try:
+    if(activate_logging):
+        handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    else:
+        handler = None
+
+    match logging_level:
+        case "DEBUG":
+            log_level = logging.DEBUG
+        case "INFO":
+            log_level = logging.INFO
+        case "WARN":
+            log_level = logging.WARNING
+        case "ERROR":
+            log_level = logging.ERROR
+        case "CRIT":
+            log_level = logging.CRITICAL
+
+except:
+    print('There is an issue with the config.yaml file. Please check thy syntax!')
+    print('Logging has been disabled!')
+    handler = None
+    log_level = logging.INFO
+
+# Successful connection to Discord
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user} with ID: {client.application_id}')
 
+# Received message in any chat
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -32,4 +57,8 @@ async def on_message(message):
 
     pass
 
-client.run(config['app']['discord_api']['token'], log_handler=handler)
+client.run(
+    config['app']['discord_api']['token'],
+    log_handler=handler,
+    log_level=log_level
+    )
