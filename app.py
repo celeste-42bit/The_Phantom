8e10dfcc2ffd59299f1614ae53e840574332f7e9
caelestia-42bit <studi.jasha.gruener@gmail.com>
2024-05-13 20:05:01 +0200

    Added sharding options to client() and config

--- app.py
+++ app.py
@@ -18,32 +18,35 @@ intents.members = True
 
 activity = discord.Activity(name="over this server", type=discord.ActivityType.watching)
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(
+    command_prefix='$',
+    intents=intents,
+    activity=activity,
+    shard_count=config['app']['discord_api']['shard_count'],
+    shard_id=config['app']['discord_api']['shard_id']
+    )
 
 # Following lines set up the logger
-activate_logger = config['app']['logging']['activate']
-level = str(config['app']['logging']['level'])
-
-
-if activate_logger:
+handler = None
+level = None
+if config['app']['logging']['activate']:
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    logging.basicConfig(level=level, handlers=[handler])
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f'Pong!\n\nI am a bot, logged in as "{bot.user.name}" with ID: {bot.user.id}.\nI am using the Discord API version {discord.__version__}.\nAnd how are you doing, human?')
+    level = str(config['app']['logging']['level'])
+    # Alternative/backup logging (internal, using python3-logging)
+    # logging.basicConfig(level=level, handlers=[handler])
 
 @bot.event
 async def on_ready():
     print(f'Connected to Discord as {bot.user.name} with ID: {bot.user.id}')
     print(f'Running API version {discord.__version__}')
+    print(f'Logging: {config["app"]["logging"]["activate"]}, Level: {level}')
 
-bot.run(config['app']['discord_api']['token'])
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong!\n\nI am a bot, logged in as "{bot.user.name}" with ID: {bot.user.id}.\nI am using the Discord API version {discord.__version__}.\nAnd how are you doing, human?')
 
-# Run
-#client.run(
-#    config['app']['discord_api']['token'],
-#    log_handler=handler,
-#    log_level=log_level
-#    )
+bot.run(
+    config['app']['discord_api']['token'],
+    log_handler=handler,
+    log_level=level
+    )
