5dc78f423cd6224d71e592145fbd7ca9bef172b7
caelestia-42bit <studi.jasha.gruener@gmail.com>
2024-05-15 13:01:25 +0200

    Renamed config.yaml.template to template_config.yaml, for file extension recognition and updated the installers

--- installer.sh
+++ installer.sh
@@ -13,7 +13,7 @@ python3 -m venv env
 source ./env/bin/activate
 python3 -m pip install -r ./requirements.txt
 
-mv ./config.yaml.template ./config.yaml
+mv ./template_config.yaml ./config.yaml
 
 python3 -m ./installer.py
 
